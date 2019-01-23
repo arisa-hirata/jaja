26abd34bf4358b62dd595eb0515ec81d6a2ac84e
April <myschoolapril@gmail.com>
2019-01-22 21:50:35 -0800

    Main Navigation

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/products">Phonecases</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/signup">SignUp</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import SignUp from './views/SignUp.vue'
+import Products from './views/Products.vue'
 
 Vue.use(Router)
 
@@ -12,6 +15,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: Products
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
     },
     {
       path: '/about',
