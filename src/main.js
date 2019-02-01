f24aa7fcd066cfcf52e832d636e7d7916349fda4
April <aprildelfino@gmail.com>
2019-01-31 16:58:55 -0800

    Bootstrap yay

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import firebase from 'firebase'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 library.add(faCoffee)
 
@@ -12,18 +17,15 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
-
-
 const config = {
-  apiKey: "AIzaSyD8WSvyqRBJdb-5KYKyVWNM7L2lYKTQ-dE",
-  authDomain: "jaja-97cfc.firebaseapp.com",
-  databaseURL: "https://jaja-97cfc.firebaseio.com",
-  projectId: "jaja-97cfc",
-  storageBucket: "jaja-97cfc.appspot.com",
-  messagingSenderId: "757251250338"
+  apiKey: 'AIzaSyD8WSvyqRBJdb-5KYKyVWNM7L2lYKTQ-dE',
+  authDomain: 'jaja-97cfc.firebaseapp.com',
+  databaseURL: 'https://jaja-97cfc.firebaseio.com',
+  projectId: 'jaja-97cfc',
+  storageBucket: 'jaja-97cfc.appspot.com',
+  messagingSenderId: '757251250338'
 }
-firebase.initializeApp(config);
-
+firebase.initializeApp(config)
 
 new Vue({
   router,
