4a159cdd16c3e4a64bcf75cf321cf4b884644d7d
JunoG <tychojun116@gmail.com>
2019-02-18 19:25:25 -0800

    review page and style

--- src/components/Phonecase.vue
+++ src/components/Phonecase.vue
@@ -35,6 +35,125 @@
         </div>
       </div>
 
+  
+
+    <div class="container">
+    			
+		<div class="row">
+			<div class="col-sm-5">
+				<div class="rating-block">
+					<h4>Average user rating</h4>
+					<h2 class="bold padding-bottom-7">4.3 <small>/ 5</small></h2>
+					<button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
+					  <i class="fas fa-star"></i>
+					</button>
+					<button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
+					  <i class="fas fa-star"></i>
+					</button>
+					<button type="button" class="btn btn-warning btn-sm" aria-label="Left Align">
+					  <i class="fas fa-star"></i>
+					</button>
+					<button type="button" class="btn btn-default btn-grey btn-sm" aria-label="Left Align">
+					<i class="fas fa-star"></i>
+					</button>
+					<button type="button" class="btn btn-default btn-grey btn-sm" aria-label="Left Align">
+					  <i class="fas fa-star"></i>
+					</button>
+				</div>
+			</div>
+			<div class="col-sm-7">
+				<div class="review-block">
+					<div class="row">
+						<div class="col-sm-3">
+							<img src="http://dummyimage.com/60x60/666/ffffff&text=No+Image" class="img-rounded">
+							<div class="review-block-name"><a href="#">Tim</a></div>
+						
+						</div>
+						<div class="col-sm-9">
+							<div class="review-block-rate">
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+							</div>
+							<div class="review-block-title">The best!</div>
+							<div class="review-block-description">Beautiful!</div>
+						</div>
+					</div>
+					<hr/>
+					<div class="row">
+						<div class="col-sm-3">
+							<img src="http://dummyimage.com/60x60/666/ffffff&text=No+Image" class="img-rounded">
+							<div class="review-block-name"><a href="#">Jay</a></div>
+							
+						</div>
+						<div class="col-sm-9">
+							<div class="review-block-rate">
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								 <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+							</div>
+							<div class="review-block-title">this was nice</div>
+							<div class="review-block-description">this was nice</div>
+						</div>
+					</div>
+					<hr/>
+					<div class="row">
+						<div class="col-sm-3">
+							<img src="http://dummyimage.com/60x60/666/ffffff&text=No+Image" class="img-rounded">
+							<div class="review-block-name"><a href="#">Lee</a></div>
+							
+						</div>
+						<div class="col-sm-9">
+							<div class="review-block-rate">
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+								<button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+								  <i class="fas fa-star"></i>
+								</button>
+							</div>
+							<div class="review-block-title">Excellent!</div>
+							<div class="review-block-description">I liked it!</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>			
+		
+    </div> <!-- /container -->
 
     </div>
   </section>
@@ -148,6 +267,54 @@ h4 {
     left: 30px;
   }
 }
+
+.btn-grey{
+    background-color:#D8D8D8;
+	color:#FFF;
+}
+.rating-block{
+	background-color:#FAFAFA;
+	border:1px solid #EFEFEF;
+	padding:15px 15px 20px 15px;
+	border-radius:3px;
+}
+.bold{
+	font-weight:700;
+}
+.padding-bottom-7{
+	padding-bottom:7px;
+}
+
+.review-block{
+	background-color:#FAFAFA;
+	border:1px solid #EFEFEF;
+	padding:15px;
+	border-radius:3px;
+	margin-bottom:15px;
+}
+.review-block-name{
+	font-size:12px;
+	margin:10px 0;
+}
+.review-block-date{
+	font-size:12px;
+}
+.review-block-rate{
+	font-size:13px;
+	margin-bottom:15px;
+}
+.review-block-title{
+	font-size:15px;
+	font-weight:700;
+	margin-bottom:10px;
+}
+.review-block-description{
+	font-size:13px;
+  text-align: left;
+}
+
+
+
 </style>
 <script>
 export default {
